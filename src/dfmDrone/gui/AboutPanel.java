211dac8237ab0bf120f60a59f3ffe72bfb43bcee
Lasse Holm Nielsen S123954 <Lasse H Nielsen@DTU>
2017-06-15 01:28:34 +0200

    [MOD] Lavet links i about panel og tilfjet lidt ekstra tekst. License billedet er også ændret til den fulde størrelse. [MOD] Ryddet lidt op i koden i help panel.

--- src/dfmDrone/gui/AboutPanel.java
+++ src/dfmDrone/gui/AboutPanel.java
@@ -1,11 +1,21 @@
 package dfmDrone.gui;
+import dfmDrone.utils.DFMLogger;
+import java.awt.Color;
+import java.awt.Desktop;
+import java.net.MalformedURLException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.util.logging.Level;
 import javax.swing.Icon;
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JTextPane;
 import javax.swing.SwingUtilities;
 import javax.swing.text.BadLocationException;
 import javax.swing.text.DefaultCaret;
+import javax.swing.text.Element;
 import javax.swing.text.SimpleAttributeSet;
 import javax.swing.text.Style;
 import javax.swing.text.StyleConstants;
@@ -19,8 +29,7 @@ import javax.swing.text.StyledDocument;
  */
 public class AboutPanel extends javax.swing.JPanel
 {
-    private int start = 0;
-    private int end = 0;
+    private static final String URL_ATT_NAME = "URL"; 
     
     public AboutPanel() {
         initComponents();
@@ -33,19 +42,30 @@ public class AboutPanel extends javax.swing.JPanel
         StyleContext context = new StyleContext();
         Style labelStyle = context.getStyle(StyleContext.DEFAULT_STYLE);
         
+        //Hyperlinks
+        Style urlStyle = doc.addStyle(null, labelStyle); 
+        StyleConstants.setForeground(urlStyle, Color.BLUE); 
+        StyleConstants.setAlignment(urlStyle, StyleConstants.ALIGN_CENTER);
+        StyleConstants.setFontSize(urlStyle, 11);
+        urlStyle.addAttribute(URL_ATT_NAME, new Object()); 
+        
+        //Title
         SimpleAttributeSet title = new SimpleAttributeSet();
         StyleConstants.setAlignment(title, StyleConstants.ALIGN_CENTER);
         StyleConstants.setBold(title, true);
         StyleConstants.setFontSize(title, 13);
         
+        //Highlight text
         SimpleAttributeSet highlight = new SimpleAttributeSet();
         StyleConstants.setAlignment(highlight, StyleConstants.ALIGN_CENTER);
         StyleConstants.setFontSize(highlight, 12);
         
+        //Normal text
         SimpleAttributeSet normal = new SimpleAttributeSet();
         StyleConstants.setAlignment(normal, StyleConstants.ALIGN_CENTER);
         StyleConstants.setFontSize(normal, 11);
         
+        //Icons
         Icon logo = new ImageIcon(getClass().getResource("/dfmDrone/gui/symbols/dfm_logo_36.png"));
         JLabel logoLabel = new JLabel(logo);
         StyleConstants.setComponent(labelStyle, logoLabel);
@@ -55,12 +75,20 @@ public class AboutPanel extends javax.swing.JPanel
         
         try {
             doc.insertString(0, "Logo", labelStyle);
-            start = doc.getLength();
             addText(getAboutProject(), highlight, doc);
             addText("\n-- Creators --\n", title, doc);
             addText(getAboutUs(), normal, doc);
             addText("\n-- License --\n", title, doc);
-            addText(getAboutLicense(), normal, doc);
+            addText(getAboutLicenseYADrone(), normal, doc);
+            doc.insertString(doc.getLength(), "vsis-www.informatik.uni-hamburg.de/oldServer/teaching/projects/yadrone/\n", urlStyle);
+            addText("Licensed under Apache License 2.0\n", normal, doc);
+            doc.insertString(doc.getLength(), "apache.org/licenses/LICENSE-2.0\n", urlStyle);
+            addText(getAboutLicenseIcons8(), normal, doc);
+            doc.insertString(doc.getLength(), "icons8.com\n", urlStyle);
+            addText("Licensed under Creative Commons BY-ND 3.0\n", normal, doc);
+            doc.insertString(doc.getLength(), "creativecommons.org/licenses/by-nd/3.0/\n", urlStyle);
+            addText(getAboutLicenseCode(), normal, doc);
+            doc.insertString(doc.getLength(), "creativecommons.org/licenses/by-nc-sa/4.0/\n", urlStyle);
             StyleConstants.setComponent(labelStyle, licenseLabel);
             doc.insertString(doc.getLength(), "License", labelStyle);
         } catch (BadLocationException ex) {
@@ -69,18 +97,18 @@ public class AboutPanel extends javax.swing.JPanel
     }
     
     private void addText(String text, SimpleAttributeSet style, StyledDocument doc) throws BadLocationException {
-        end =+ text.length() + 1;
-        doc.insertString(doc.getLength(), text, style);
+        int start = doc.getLength();
+        int end = start + text.length() + 1;
+        doc.insertString(start, text, style);
         doc.setParagraphAttributes(start, end, style, true);
-        start = end;
     }
     
     private static String getAboutProject() {
         StringBuilder sb = new StringBuilder();
         
         sb.append("\nDFM Drone Client version: 1.0 (June 2017)\n");
-        sb.append("Drone client for the Parrot AR2.0 Drone\n");
-        sb.append("This project is a result of a group project at the Technical Univercity of Denmark (DTU)\n");
+        sb.append("Drone client for the Parrot AR2.0 Drone autonomous flight\n");
+        sb.append("This project is the result of a group project at the Technical Univercity of Denmark (DTU)\n");
         
         return sb.toString();
     }
@@ -103,13 +131,30 @@ public class AboutPanel extends javax.swing.JPanel
         return sb.toString();
     }
     
-    private static String getAboutLicense() {
+    private static String getAboutLicenseYADrone() {
         StringBuilder sb = new StringBuilder();
         
-        sb.append("Icons:\n");
-        sb.append("Artist: Icons8\nWebsite: icons8.com\n");
+        sb.append("Drone API:\n");
+        sb.append("Owner: YADrone\nWebsite: ");
+        
+        return sb.toString();
+    }
+    
+    private static String getAboutLicenseIcons8() {
+        StringBuilder sb = new StringBuilder();
+        
+        sb.append("\nIcons:\n");
+        sb.append("Artist: Icons8\nWebsite: ");
+        
+        return sb.toString();
+    }
+    
+    private static String getAboutLicenseCode() {
+        StringBuilder sb = new StringBuilder();
+        
         sb.append("\nCode:\n");
-        sb.append("Licensed under Creative Commons BY-NC-SA 4.0\nWebsite: creativecommons.org/licenses/by-nc-sa/4.0/\n");
+        sb.append("Owner: DFM Group\n");
+        sb.append("Licensed under Creative Commons BY-NC-SA 4.0\n");
         
         return sb.toString();
     }
@@ -131,9 +176,16 @@ public class AboutPanel extends javax.swing.JPanel
         jLabel1.setText("About   ");
         jLabel1.setFocusable(false);
 
+        jScrollPane2.setBorder(javax.swing.BorderFactory.createEtchedBorder());
+
         jtpContent.setEditable(false);
         jtpContent.setBackground(new java.awt.Color(240, 240, 240));
-        jtpContent.setFocusable(false);
+        jtpContent.setBorder(null);
+        jtpContent.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                jtpContentMouseClicked(evt);
+            }
+        });
         jScrollPane2.setViewportView(jtpContent);
 
         jPanel1.setLayout(new java.awt.GridLayout(1, 0));
@@ -179,6 +231,42 @@ public class AboutPanel extends javax.swing.JPanel
         JFrame frame = (JFrame) SwingUtilities.getWindowAncestor(this);
         frame.dispose();
     }//GEN-LAST:event_jbExitActionPerformed
+
+    private void jtpContentMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jtpContentMouseClicked
+        JTextPane textPane = (JTextPane) evt.getComponent();
+        StyledDocument doc = textPane.getStyledDocument();
+        Element elem = doc.getCharacterElement(textPane.viewToModel(evt.getPoint()));
+        
+        if (!elem.getAttributes().isDefined(URL_ATT_NAME))
+            return;
+        
+        int len = elem.getEndOffset() - elem.getStartOffset();
+        final String url;
+        
+        try {
+            url = doc.getText(elem.getStartOffset(), len);
+        } catch (BadLocationException e) {
+            DFMLogger.logger.log(Level.WARNING, "can't get URL", e);
+            return;
+        }
+        
+        Runnable run = () -> {
+            try {
+                URI uri = new URL("http://" + url).toURI();
+                Desktop desktop = Desktop.isDesktopSupported() ? Desktop.getDesktop() : null;
+                if (desktop != null && desktop.isSupported(Desktop.Action.BROWSE)) {
+                    try {
+                        desktop.browse(uri);
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                }
+            } catch (URISyntaxException | MalformedURLException e) {
+                e.printStackTrace();
+            }
+        };
+        new Thread(run).start();
+    }//GEN-LAST:event_jtpContentMouseClicked
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JLabel jLabel1;
